8299fae098101d7310e2980835cce55cb933d7e1
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-08-08 15:22:06 -0300

    Hello world

--- Vendas/src/main/java/br/com/github/brunocs/VendasApplication.java
+++ Vendas/src/main/java/br/com/github/brunocs/VendasApplication.java
@@ -2,10 +2,18 @@ package br.com.github.brunocs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
 
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
