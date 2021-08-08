9754750f9315f7c3a044d94318d23911880a3ad1
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-08-08 15:15:33 -0300

    configurações iniciais pos criação projeto

---
+++ Vendas/src/test/java/br/com/github/brunocs/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.brunocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
